de8f180182645d5efc6cfc8cc234cc620d0c8369
Cameron Reikes <cameronreikes@gmail.com>
2017-11-04 19:01:57 -0700

    Added the function to fix a directory name

--- include/operomnia1/file.h
+++ include/operomnia1/file.h
@@ -3,4 +3,7 @@
 
 bool ends_with( const char *str, const char *suffix );
 
+// Adds a slash at the end of a directory name
+char * fix_directory( const char * in_str );
+
 #endif
--- src/core/file.c
+++ src/core/file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <operomnia1/memory.h>
 #include <operomnia1/file.h>
 
 bool ends_with(const char *str, const char *suffix) {
@@ -11,4 +12,18 @@ bool ends_with(const char *str, const char *suffix) {
     if (lensuffix >  lenstr)
         return 0;
     return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+char * fix_directory( const char * in_str ) {
+  char * to_prepend;
+  if( in_str[strlen(in_str)-1] == '/' ) {
+    to_prepend = op_malloc( strlen(in_str) );
+    strcpy( to_prepend, in_str );
+  } else {
+    to_prepend = op_malloc( strlen(in_str) + 1+1 );
+    strcpy( to_prepend, in_str );
+    to_prepend[ strlen(to_prepend) ] = '/';
+    to_prepend[ strlen(to_prepend)+1 ] = '\0';
+  }
+  return to_prepend;
 }
