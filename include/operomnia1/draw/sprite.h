924a5978a11688702e557e397f8324eb21a94c49
Cameron Reikes <cameronreikes@gmail.com>
2017-11-04 19:00:56 -0700

    Made it so sprites are in the form of a linked list

--- include/operomnia1/draw/sprite.h
+++ include/operomnia1/draw/sprite.h
@@ -4,19 +4,23 @@
 #include <allegro5/allegro.h>
 #include <operomnia1/timers.h>
 
+typedef ALLEGRO_BITMAP op_bitmap;
+
 typedef struct frame {
-  ALLEGRO_BITMAP * bitmap;
+  op_bitmap * frame_data;
   char * frame_name;
   struct frame * next_frame;
 } frame;
 
 typedef struct sprite {
   op_timer * anim_timer;
+  int current_frame;
   int amount_frames;
   frame * frames;
 } sprite;
 
 sprite * load_sprite( const char * sprite_dir, float in_fps );
 
+void append_frame( frame * head_frame, char * in_frame_path );
 
 #endif
--- src/draw/sprite.c
+++ src/draw/sprite.c
@@ -2,45 +2,72 @@
 #include <dirent.h>
 #include <operomnia1/memory.h>
 #include <operomnia1/draw/draw.h>
+#include <operomnia1/draw/image.h>
 #include <operomnia1/draw/sprite.h>
 #include <operomnia1/file.h>
 
-void append_frame( frame * head_frame, const char * in_frame_name ) {
+frame * create_header_frame( char * in_frame_path ) {
+  // Create the initial memory
+  frame * to_return = op_malloc( sizeof *to_return );
+  //Initialize the values
+  assert( to_return->frame_data = al_load_bitmap( in_frame_path ) );
+  to_return->frame_name = op_malloc( strlen(in_frame_path) );
+  strcpy( to_return->frame_name, in_frame_path );
+  to_return->next_frame = NULL;
+  return to_return;
+}
+
+void append_frame( frame * head_frame, char * in_frame_path ) {
+
+  // Create the main frame to append
   frame * to_append = op_malloc( sizeof *to_append );
+  // Set the frame's path to the input path
+  to_append->frame_name = op_malloc( strlen( in_frame_path ) );
+  strcpy( to_append->frame_name, in_frame_path );
+  // Make sure that the directory is valid
+  assert( to_append->frame_data = al_load_bitmap( in_frame_path ) );
+  // Create the pointer to traverse the linked list of frames
   frame * data = head_frame;
+  // Traverse the linked list
+  while( data->next_frame != NULL ) {
+    data = data->next_frame;
+  }
+  // Actually append the frame
+  data->next_frame = to_append;
 }
 
 sprite * load_sprite( const char * sprite_dir, float in_fps ) {
+  // Create the initial memory for the sprite
   sprite * to_return = op_malloc( sizeof *to_return );
+  // Set some base values
+  to_return->current_frame = 0;
+  to_return->amount_frames = 0;
+  to_return->frames = NULL;
 
-  char * to_prepend;
-  if( sprite_dir[strlen(sprite_dir)-1] == '/' ) {
-    to_prepend = op_malloc( strlen(sprite_dir) );
-    strcpy( to_prepend, sprite_dir );
-  } else {
-    to_prepend = op_malloc( strlen(sprite_dir) + 1+1 );
-    strcpy( to_prepend, sprite_dir );
-    to_prepend[ strlen(to_prepend) ] = '/';
-    to_prepend[ strlen(to_prepend)+1 ] = '\0';
-  }
+  // Make the directory better
+  char * to_prepend = fix_directory( sprite_dir );
   printf( "Opening dir %s\n", to_prepend );
-
+  // Basic datatypes to look in the directory
   DIR *dir;
-  int frames = 0;
   struct dirent *ent;
+  // If the directory exists
   if( (dir = opendir( sprite_dir ) ) != NULL ) {
+    // While the file in the directory isn't null
     while( (ent = readdir (dir) ) != NULL ) {
       printf( "Reading file %s\n", ent->d_name );
+      // If it's a .png file
       if( ends_with(ent->d_name, ".png" ) ) {
         printf( "Frame found at %s\n", ent->d_name );
+        // Create the string for the frame to laod
         char * result = op_malloc( strlen(ent->d_name)+strlen(to_prepend)+1 );
         strcpy( result, to_prepend );
         strcat( result, ent->d_name );
         printf( "Appending frame %s...\n", result );
-        frames += 1;
-
-
-
+        if( to_return->frames == NULL ) {
+          to_return->frames = create_header_frame( result );
+        } else {
+          append_frame( to_return->frames, result );
+        }
         op_free( result );
       }
     }
