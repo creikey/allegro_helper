81101bdd2602c63ed92b94903e5005c4e1542d0b
Cameron Reikes <cameronreikes@gmail.com>
2017-11-10 19:55:29 -0800

    Added frame numbers and fps

--- include/operomnia1/draw/sprite.h
+++ include/operomnia1/draw/sprite.h
@@ -8,6 +8,7 @@ typedef ALLEGRO_BITMAP op_bitmap;
 
 typedef struct frame {
   op_bitmap * frame_data;
+  int frame_numb;
   char * frame_name;
   struct frame * next_frame;
 } frame;
@@ -16,11 +17,13 @@ typedef struct sprite {
   op_timer * anim_timer;
   int current_frame;
   int amount_frames;
+  float fps;
   frame * frames;
 } sprite;
 
 void read_frames( frame * head_frame );
 
+// You just need a number before the .png
 sprite * load_sprite( const char * sprite_dir, float in_fps );
 
 void append_frame( frame * head_frame, char * in_frame_path );
