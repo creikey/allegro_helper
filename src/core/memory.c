5d0874e5d9fb37cf67935618164ca6b521cf07f2
Cameron Reikes <cameronreikes@gmail.com>
2017-11-10 19:04:01 -0800

    Added cross platform calloc

--- src/core/memory.c
+++ src/core/memory.c
@@ -5,6 +5,10 @@ void * op_malloc( size_t to_malloc ) {
   return al_malloc( to_malloc );
 }
 
+void * op_calloc( size_t n_items, size_t s_items ) {
+  return al_calloc( n_items, s_items );
+}
+
 void op_free( void * to_free ) {
   al_free( to_free );
 }
