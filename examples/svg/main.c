a9a0e553119608918535f3408dde5617c95358a2
Cameron Reikes <cameronreikes@gmail.com>
2017-12-04 19:45:43 -0800

    Added svg example

--- examples/svg/main.c
+++ examples/svg/main.c
@@ -36,14 +36,18 @@ int main( void ) {
 }
 
 svg * main_svg;
+vector svg_pos;
 
 void init_game( operomnia_data * in_data ) {
-  main_svg = load_svg( "tiger.svg" );
+  svg_pos = v(0,0);
+  main_svg = load_svg( "World_V2.0.svg", STANDARD_SVG_RES );
   printf( "Svg dimensions: %f, %f\n", get_svg_dimensions(main_svg).x, get_svg_dimensions(main_svg).y );
 }
 
 void game_loop( operomnia_data * in_data ) {
   clear_color( default_clear_color );
-
+  //combine_vector(&svg_pos,v(1,1));
+  scale_svg( main_svg, add_vectors(get_svg_scale(main_svg), v(0.005,0.01) ) );
+  draw_svg( main_svg, new_color(255,255,255), svg_pos, 1 );
   draw_all();
 }
